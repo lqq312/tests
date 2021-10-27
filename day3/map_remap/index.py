2b7964df67ab23dde6a17723306f55f7f9903934
luqq <luqingqing@ycig.com>
2021-10-28 06:27:18 +0800

    delete all

--- day3/map_remap/index.py
+++
@@ -1,9 +0,0 @@
-# encoding=utf-8
-from importlib._bootstrap import __import__
-
-Modu_Name="modu1"
-Func="f1"
-Modu1=__import__(Modu_Name)
-
-F1=getattr(Modu1,Func)
-print(F1())
--- day3/map_remap/modu2.py
+++
@@ -1,4 +0,0 @@
-# encoding=utf-8
-
-def f1():
-    return 2
